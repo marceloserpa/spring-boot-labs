1d38a8efbf8791252e54735a65713716f0d2a21e
MarceloILegra <marcelo.serpa@ilegra.com>
2015-05-10 17:51:24 -0300

    Add test profile in .yaml and add h2 database

--- spring-boot-jpa-sample/src/test/java/br/com/lendingofmaterials/controller/MaterialControllerTest.java
+++ spring-boot-jpa-sample/src/test/java/br/com/lendingofmaterials/controller/MaterialControllerTest.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -13,11 +14,13 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = App.class)
+@ActiveProfiles("test")
 @WebAppConfiguration
 public class MaterialControllerTest {
 
@@ -30,7 +33,14 @@ public class MaterialControllerTest {
     }   
     
     @Test
-    public void listAllMaterialsTest() throws Exception{
+    public void addNewMaterialTest() throws Exception{            
+       this.mockMvc.perform(post("/materiais/novo").param("nome", "material 1").param("estoque", "5").param("dataCompra", "2013-12-12"))
+                .andDo(print())
+                .andExpect(status().isOk()); 
+    }
+    
+    @Test
+    public void listAllMaterialsTest() throws Exception{                
         this.mockMvc.perform(get("/materiais"))
                 .andDo(print())
                 .andExpect(status().isOk());
