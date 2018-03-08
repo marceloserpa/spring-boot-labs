ac3cbbd8b34606fb470403d4ebaac83f24300b85
marceloserpa <jdev.marcelo@gmail.com>
2018-03-08 20:46:57 -0300

    poc: added kafka producer health checker

--- spring-boot-actuator-healthcheck-kafka/src/main/java/br/com/marceloserpa/actuator/kafka/poc/healthchecks/KafkaProducerHealthIndicator.java
+++ spring-boot-actuator-healthcheck-kafka/src/main/java/br/com/marceloserpa/actuator/kafka/poc/healthchecks/KafkaProducerHealthIndicator.java
@@ -1,16 +1,16 @@
 package br.com.marceloserpa.actuator.kafka.poc.healthchecks;
 
-import br.com.marceloserpa.actuator.kafka.poc.kafka.KafkaConsumerHealthChecker;
+import br.com.marceloserpa.actuator.kafka.poc.kafka.KafkaProducerHealthChecker;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
 
 @Component
-public class KafkaConsumerHealthIndicator implements HealthIndicator {
+public class KafkaProducerHealthIndicator implements HealthIndicator {
 
     @Override
     public Health health() {
-        boolean isUp = new KafkaConsumerHealthChecker().check();
+        boolean isUp = new KafkaProducerHealthChecker().check();
         return isUp ? Health.up().build() : Health.outOfService().build();
     }
 
